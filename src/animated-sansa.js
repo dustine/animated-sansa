0680ef404d733f2bc01ae5576c932a5f55be8672
Dustine Camacho <dustine.camacho@gmail.com>
2015-06-12 19:49:34 +0100

    Last setting ups

---
+++ src/animated-sansa.js
@@ -0,0 +1,16 @@
+var Crafty = require('craftyjs')
+var $ = require('jquery')
+
+Crafty.init(600, 300, 'game')
+
+var game = $('#game')
+game.on('click', function () {
+  this.requestPointerLock()
+})
+
+Crafty.background('rgb(127,127,127)')
+
+Crafty.e("Paddle, 2D, DOM, Color, Multiway")
+    .color('green')
+    .attr({ x: 580, y: 100, w: 10, h: 100 })
+    .multiway(4, { UP_ARROW: -90, DOWN_ARROW: 90 });
