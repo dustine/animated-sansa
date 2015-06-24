9b9486a88916f44e673a9801e5daa05edd7eb371
Dustine Camacho <dustine.camacho@gmail.com>
2015-06-24 18:04:13 +0100

    Okay, let's try treating them like binary files
    And by them I mean the browserified js inside dist/

--- src/animated-sansa.js
+++ src/animated-sansa.js
@@ -1,7 +1,7 @@
 var Crafty = require('craftyjs')
 var $ = require('jquery')
 
-Crafty.init(600, 300, 'game')
+Crafty.init(600, 300, '')
 
 var game = $('#game')
 game.on('click', function () {
@@ -10,7 +10,7 @@ game.on('click', function () {
 
 Crafty.background('rgb(127,127,127)')
 
-Crafty.e("Paddle, 2D, DOM, Color, Multiway")
+Crafty.e('Paddle, 2D, DOM, Color, Multiway')
     .color('green')
     .attr({ x: 580, y: 100, w: 10, h: 100 })
-    .multiway(4, { UP_ARROW: -90, DOWN_ARROW: 90 });
+    .multiway(4, { UP_ARROW: -90, DOWN_ARROW: 90 })
