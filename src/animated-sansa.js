b90ccb33b52dd2c5a7cf4631889fd128cd2f5fbd
Dustine Camacho <dustine.camacho@gmail.com>
2015-06-24 22:36:23 +0100

    HTML5 boilerplate insert, some pretties

--- src/animated-sansa.js
+++ src/animated-sansa.js
@@ -1,16 +1,20 @@
 var Crafty = require('craftyjs')
-var $ = require('jquery')
+var $ = window.$
 
-Crafty.init(600, 300, 'game')
+var width = 600
+var height = 400
 
-var game = $('#game')
+Crafty.init(width, height, 'game')
+
+var game = $('.game-container')
 game.on('click', function () {
   this.requestPointerLock()
 })
 
-Crafty.background('rgb(127,127,127)')
+Crafty.background('rgb(0,0,0)')
 
-Crafty.e('Paddle, 2D, DOM, Color, Multiway')
+Crafty.e('Paddle, 2D, DOM, Color, Fourway')
     .color('red')
-    .attr({ x: 580, y: 100, w: 10, h: 100 })
-    .multiway(4, { UP_ARROW: -90, DOWN_ARROW: 90 })
+    .css('border-radius', '100%')
+    .attr({ x: width / 2 - 5, y: height / 2 - 5, w: 10, h: 10 })
+    .fourway(4)
