25ddf7f201f75716741d856d122776338302584e
Dustine Camacho <dustine.camacho@gmail.com>
2015-06-25 12:02:29 +0100

    mouse controls implemented

--- src/animated-sansa.js
+++ src/animated-sansa.js
@@ -1,20 +1,62 @@
 var Crafty = require('craftyjs')
-var $ = window.$
+var $ = require('jquery')
 
+var debug = true
 var width = 600
 var height = 400
 
 Crafty.init(width, height, 'game')
+Crafty.background('rgb(0,0,0)')
 
-var game = $('.game-container')
+// player particle
+var player = Crafty.e('Current, 2D, DOM, Color, Fourway')
+  .color('green')
+  .css('border-radius', '100%')
+  .attr({ x: width / 2 - 5, y: height / 2 - 5, w: 10, h: 10 })
+  .fourway(4)
+
+if (debug) {
+  player.addComponent('Keyboard')
+  player.bind('KeyDown', function (ke) {
+    if (ke.key === Crafty.keys.R) {
+      this.x = width / 2 - 5
+      this.y = height / 2 - 5
+    } else if (ke.key === Crafty.keys.W) {
+      console.log(this.x, this.y)
+    }
+  })
+}
+
+// mouse controls
+
+var game = $(Crafty.stage.elem)
 game.on('click', function () {
   this.requestPointerLock()
 })
 
-Crafty.background('rgb(0,0,0)')
+// document.addEventListener('pointerlockerror', lockError, false)
 
-Crafty.e('Paddle, 2D, DOM, Color, Fourway')
-    .color('red')
-    .css('border-radius', '100%')
-    .attr({ x: width / 2 - 5, y: height / 2 - 5, w: 10, h: 10 })
-    .fourway(4)
+// TODO Remove or rework this later
+function lockError () {
+  console.log('Pointer lock failed')
+  player.fourway(4)
+}
+
+function lockSuccess () {
+  if (document.pointerLockElement === Crafty.stage.elem) {
+    Crafty.addEvent(player, Crafty.stage.elem, 'mousemove', player.mouseMove)
+    player.fourway(0)
+  } else {
+    Crafty.removeEvent(player, Crafty.stage.elem, 'mousemove', player.mouseMove)
+    player.fourway(4)
+  }
+  console.log('pointerlockchange')
+}
+
+player.mouseMove = function (me) {
+  this.x += me.movementX
+  this.y += me.movementY
+}
+
+$(document).on('pointerlockchange', lockSuccess)
+$(document).on('pointerlockerror', lockError)
