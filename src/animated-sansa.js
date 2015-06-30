968f060bb4fd58c903feb9694b6cd7f23ce203df
Dustine Camacho <dustine.camacho@gmail.com>
2015-06-30 14:03:03 +0100

    Collision works, for real!!!!
    Had to implement my own controller and then overwrite the Fourkey one so
    it respected the borders. Also trashed collision completely, but the way
    I did it could easily be modified to work with it!!

--- src/animated-sansa.js
+++ src/animated-sansa.js
@@ -2,57 +2,128 @@ var Crafty = require('craftyjs')
 // var $ = require('jquery')
 var $ = window.$
 
-var debug = true
-var width = 600
-var height = 400
+// var debug = true
 
-Crafty.init(width, height, 'game')
+// global consts
+var WIDTH = 600
+var HEIGHT = 400
+var BORDER = 20
+
+// player constants
+var MAX_SPEED = 8
+var RADIUS = 4.5
+
+Crafty.init(WIDTH, HEIGHT, 'game')
 Crafty.background('rgb(0,0,0)')
 
-// borders
-Crafty.e('Wall, 2D, Canvas, Color')
-  .color('red')
-  .attr({x: 0, y: 0, h: 20, w: width})
-Crafty.e('Wall, 2D, Canvas, Color')
-  .color('red')
-  .attr({x: 0, y: 0, h: height, w: 20})
-Crafty.e('Wall, 2D, Canvas, Color')
-  .color('red')
-  .attr({x: 0, y: height - 20, h: 20, w: width})
-Crafty.e('Wall, 2D, Canvas, Color')
-  .color('red')
-  .attr({x: width - 20, y: 0, h: height, w: 20})
+// mouse control
+Crafty.c('PointerWay', {
+  init: function () {
+    var that = this
+    this._mouseMovement = {
+      x: 0,
+      y: 0
+    }
+    this._speed = {
+      x: 0,
+      y: 0
+    }
+    this._mouseMoveAtPointerLock = function (mouseEvent) {
+      that._mouseMovement.x += mouseEvent.movementX
+      that._mouseMovement.y += mouseEvent.movementY
+    }
+    Crafty.addEvent(Crafty.stage.elem, Crafty.stage.elem, 'mousemove', this._mouseMoveAtPointerLock)
+  },
+  remove: function () {
+    this.unbind('EnterFrame', this._enterFrame)
+    Crafty.removeEvent(Crafty.stage.elem, Crafty.stage.elem, 'mousemove', this._mouseMoveAtPointerLock)
+  },
+  _enterFrame: function () {
+    var movX = this._mouseMovement.x
+    var movY = this._mouseMovement.y
+    this._mouseMovement.x = this._mouseMovement.y = 0
+    if (Math.abs(movX) > MAX_SPEED || Math.abs(movY) > MAX_SPEED) {
+      var newMovX, newMovY
+      if (Math.abs(movX) > Math.abs(movY)) {
+        newMovX = Math.sign(movX) * MAX_SPEED
+        newMovY = movY * (MAX_SPEED / Math.abs(movX))
+      } else {
+        newMovY = Math.sign(movY) * MAX_SPEED
+        newMovX = movX * (MAX_SPEED / Math.abs(movY))
+      }
+      movX = newMovX
+      movY = newMovY
+    }
+    this.x += movX
+    this.y += movY
+
+    if (this._callback) {
+      this._callback()
+    }
+  },
+  callback: function (f) {
+    console.log('registering callback', this, f)
+    if (typeof f === 'function') {
+      this._callback = f.bind(this)
+    }
+    return this
+  },
+  speed: function (speed) {
+    if (speed.x !== undefined && speed.y !== undefined) {
+      this._speed.x = speed.x
+      this._speed.y = speed.y
+    } else {
+      this._speed.x = speed
+      this._speed.y = speed
+    }
+    return this
+  },
+  pointerway: function (speed) {
+    this.speed(speed)
+    this.bind('EnterFrame', this._enterFrame)
+    return this
+  }
+})
 
 // player particle
-var player = Crafty.e('Current, 2D, DOM, Color, Fourway, Collision')
+var player = Crafty.e('Current, 2D, DOM, Color, Collision, Fourway')
   .color('rgb(7, 124, 190)')
   .css('border-radius', '100%')
-  .attr({x: width / 2 - 5, y: height / 2 - 5, w: 10, h: 10})
+  .attr({x: WIDTH / 2 - RADIUS, y: HEIGHT / 2 - RADIUS, w: RADIUS * 2, h: RADIUS * 2})
   .fourway(4)
-  .onHit('Wall', function (hitdata) {
-    // this.shift(-this._movement.x, -this._movement.y, 0, 0)
-    if (this._movement) {
-      this.x -= this._movement.x
-      this.y -= this._movement.y
-    }
-    // this._speed.x = this._speed.y = 0
-  })
   .origin('center')
-  // .bind('Move', function (oldPosition) {
-  //   if (this.hit('Wall')) {
-  //     console.log(oldPosition, 'vs', this.pos())
-  //     // this.x = oldPosition._x
-  //     this.y = oldPosition._y + 1
-  //   }
-  // })
 
-if (debug) {
+function collision () {
+  if (this._x < BORDER) {
+    this.x = BORDER
+  } else if (this._x > WIDTH - this.w - BORDER) {
+    this.x = WIDTH - this.w - BORDER
+  }
+
+  if (this._y < BORDER) {
+    this.y = BORDER
+  } else if (this._y > HEIGHT - this.h - BORDER) {
+    this.y = HEIGHT - this.h - BORDER
+  }
+}
+
+// hack: adding collision detection to keyboard controls
+player.unbind('EnterFrame', player._enterframe)
+player.bind('EnterFrame', function () {
+  player._enterframe()
+  if (!player.disableControls) {
+    collision.apply(player)
+  }
+})
+
+if (this.debug) {
+  player.addComponent('WiredHitBox')
   player.addComponent('Keyboard')
   player.bind('KeyDown', function (ke) {
     if (ke.key === Crafty.keys.R) {
-      this.x = width / 2 - 5
-      this.y = height / 2 - 5
-    } else if (ke.key === Crafty.keys.W) {
+      this.x = WIDTH / 2 - RADIUS
+      this.y = HEIGHT / 2 - RADIUS
+    } else if (ke.key === Crafty.keys.Q) {
       console.log(this.x, this.y)
     }
   })
@@ -72,51 +143,30 @@ function lockError () {
   console.log('Pointer lock failed')
 }
 
-function lockSuccess () {
+var lockOnce = true
+
+function lockChange () {
   // TODO Only bind the EnterFrame once, it's doing every time we click
   if (document.pointerLockElement === Crafty.stage.elem) {
     // enable mouse control, delayed to prevent sudden jump from
     //  accepting the pointer lock prompt
-    player.disableControl()
+    if (!lockOnce) return
+    lockOnce = false
+    player.disableControl() // only disables Fourway
     setTimeout(function () {
-      Crafty.addEvent(player, Crafty.stage.elem, 'mousemove', updateLocalMouseMovement)
-      player.bind('EnterFrame', mouseMove)
+      player.addComponent('PointerWay')
+        .pointerway(MAX_SPEED)
+        .callback(collision)
       console.log('mouse activated')
     }, 50)
   } else {
     // reset back to keyboard control
-    Crafty.removeEvent(player, Crafty.stage.elem, 'mousemove', updateLocalMouseMovement)
-    player.unbind('EnterFrame', mouseMove)
-    player._movement.x = 0
-    player._movement.y = 0
+    lockOnce = true
+    player.removeComponent('PointerWay')
     player.enableControl()
   }
   console.log('pointerlockchange')
 }
 
-var _movX = 0, _movY = 0
-
-function updateLocalMouseMovement (me) {
-  _movX += me.movementX
-  _movY += me.movementY
-}
-
-function mouseMove () {
-  player.shift(_movX, _movY, 0, 0)
-  player._movement.x = _movX
-  player._movement.y = _movY
-  player.trigger('Move', {
-    _x: this._x - _movX,
-    _y: this._y - _movY,
-    _w: this._w,
-    _h: this._h
-  })
-  // if (this.hit('Wall')) {
-  //   player.shift(-_movX, -_movY, 0, 0)
-  //   player._speed.x = player._speed.y = 0
-  // }
-  _movX = _movY = 0
-}
-
-$(document).on('pointerlockchange', lockSuccess)
+$(document).on('pointerlockchange', lockChange)
 $(document).on('pointerlockerror', lockError)
