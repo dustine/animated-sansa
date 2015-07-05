36f2b750f23ec31ffc28b7721ffc873ceea6eca6
Dustine Camacho <dustine.camacho@gmail.com>
2015-07-05 21:42:28 +0100

    Removing leftover code

--- js/main.js
+++ js/main.js
@@ -1,19 +1,2 @@
 $(document).ready(function() {
-  function addIntegerBorderRadius(length, i, elem) {
-    i = length - i - 1;
-    if (i % 3 === 0) {
-      $(elem).addClass('-right');
-    }
-    if ((i + 1) % 3 === 0) {
-      $(elem).addClass('-left');
-    }
-  }
-
-  $('.counter.-separate').each(function(i, elem) {
-    var $children = $(elem).children();
-    var $digits = $children.filter('.digit');
-    $digits.each(function(i, elem) {
-      addIntegerBorderRadius($digits.length, i, elem);
-    });
-  });
 });
